e7a2cdc656c622c8c71dc87152e274fa937c1df1
jokerkras <krasmax97@mail.ru>
2019-10-15 10:42:29 +0300

    add Login Page

--- lib/ui/login_screen.dart
+++ lib/ui/login_screen.dart
@@ -7,19 +7,116 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  String _login;
+  bool _isLoginBtnEnabled = false;
+
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _emailController.addListener(() {
+      _login = _emailController.text;
+      setState(() {
+        _isLoginBtnEnabled = _passwordController.value.text.isNotEmpty && _emailController.value.text.isNotEmpty;
+      });
+    });
+
+    _passwordController.addListener(() {
+      setState(() {
+        _isLoginBtnEnabled = _passwordController.value.text.isNotEmpty && _emailController.value.text.isNotEmpty;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(''),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          ],
-        ),
+      backgroundColor: Color.fromARGB(255, 232, 233, 237),
+      body: Stack (
+        fit: StackFit.expand,
+        children: <Widget>[
+          Image.asset("assets/bg.png",
+          fit: BoxFit.fitWidth,
+          alignment: Alignment.topCenter,),
+          Positioned(
+            bottom: 50,
+            left: 30,
+            right: 30,
+            top: 0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Container(
+                  child: Text("Вход",
+                  style: TextStyle(
+                    fontSize: 34,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    ),
+                  ),
+                  margin: EdgeInsets.only(bottom: 40),
+                ),
+                Card(
+                  elevation: 16,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16)
+                    ),
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 60),
+                      height: 250,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            _buildTextField('Email', _emailController),
+                            _buildTextField('Пароль', _passwordController),
+                            MaterialButton(
+                              elevation: 0,
+                                minWidth: 230,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30)
+                              ),
+                              child: Text('Войти',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              color: Color.fromARGB(254, 155, 81, 224),
+                              disabledColor: Color.fromARGB(155, 155, 81, 224),
+                              onPressed: _isLoginBtnEnabled ? () => _callback() : null,
+                            )
+                          ],
+                        ),
+                    ),
+                  ),
+              ],
+            ),
+          )
+        ],
       ),
     );
+  }
+  
+  Widget _buildTextField(String text, TextEditingController controller) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(6.0),
+              borderSide: BorderSide(color: Colors.red)
+          ),
+          hintText: text,
+          labelText: text
+      ),
+    );
+  }
+
+  _callback() {
+
   }
 }
